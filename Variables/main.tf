7d8770e6d79075ca3544d625c000c77b560f79e8
itrepo <itrepo01@gmail.com>
2019-01-04 18:37:31 -0800

    update tags

--- Variables/main.tf
+++ Variables/main.tf
@@ -1,4 +1,23 @@
 provider "aws" {
   region = "us-west-2"
 }
+variable "tag" {
+  type = "map"
+  default = {
+    dev = "development-env"
+    prd = "production-env"
+    stg = "production-env"
+  }
+}
 
+variable "env" {
+  
+}
+
+
+
+
+#
+tags {
+  environment_type = "${lookup(var.tag, var.env,)}"
+}
